a11e3f10415915d3bf7b295840ef913b2f1eeba7
Horacio Garza <hhgarza95@gmail.com>
2016-05-11 01:36:31 -0500

    Isn't working YET

--- src/modeladoysimulacion/MedioCurso/GeneradorCongruencialMixto.java
+++ src/modeladoysimulacion/MedioCurso/GeneradorCongruencialMixto.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package modeladoysimulacion.MedioCurso;
+
 import ownUtils.*;
 
 /**
@@ -11,15 +12,12 @@ import ownUtils.*;
  * @author hgx95
  */
 public class GeneradorCongruencialMixto {
+
     /**
-     * Xn1 = Semilla generada
-     * a = Constante Multiplicativa
-     * X0 = Semilla Inicial
-     * C = cte. Aditiva
-     * Mod = Función obtenida del residuo de una división
-     * m = modulo (divisor constante)
+     * Xn1 = Semilla generada a = Constante Multiplicativa X0 = Semilla Inicial
+     * C = cte. Aditiva Mod = Función obtenida del residuo de una división m =
+     * modulo (divisor constante)
      */
-    
     int xn1, a, x0, c, m;
     int n; //iteraciones
     boolean isTrustable;
@@ -27,13 +25,15 @@ public class GeneradorCongruencialMixto {
     Fraction currentFraction, lastFraction, auxFraction;
     double rectNumb;
     int initialX0;
-    
+    String[][] resultsToPrint;
+
     /**
      * Gives the first parameters
+     *
      * @param a
      * @param x0
      * @param c
-     * @param m 
+     * @param m
      */
     public GeneradorCongruencialMixto(int a, int x0, int c, int m) {
         this.a = a;
@@ -43,47 +43,108 @@ public class GeneradorCongruencialMixto {
         this.isTrustable = false;
         this.rectNumb = 0.0;
         this.n = 1;
-        
+        this.resultsToPrint = null;
+
     }
-    
+
     /**
      * Initial
      */
-    public void makeThisHappen(){
-        
+    public void makeThisHappen() {
+
         initialX0 = x0;
-        
+
         for (int i = n; i <= m; i++, n++) {
-            
-          currentFraction = new Fraction((a*x0+c)%m , m);
-          xn1 = currentFraction.getNumerator();
-          rectNumb = currentFraction.evaluate();
-          x0 = xn1;
-          
-          if (xn1 == initialX0 && i != m) { 
-              break; 
-          }
-          
+
+            currentFraction = new Fraction((a * x0 + c) % m, m);
+            xn1 = currentFraction.getNumerator();
+            rectNumb = currentFraction.evaluate();
+            x0 = xn1;
+
+            setValuesOnArray(i, m, ((Integer) n).toString(), ((Integer) x0).toString(), currentFraction.toString(), ((Integer) xn1).toString(), ((Double) rectNumb).toString());
+
+            if (xn1 == initialX0 && i != m) {
+                break;
+            }
+
         }
-        
-        if (xn1 == initialX0 && n ==  (m+1)) {
+
+        if (xn1 == initialX0 && n == (m + 1)) {
             isTrustable = true;
         }
-        
+
         if (isTrustable) {
-            
+
             System.out.println("Generador Congruencial Mixto confiable");
-            
-        }
-        else{
+
+        } else {
             System.out.println("Generador Congruencial Mixto no confiable");
         }
-        
+
     }
-    
-    
-    
-    
-    
-    
+
+    public String letsPrintThisShit() {
+        String returningTheArray = "";
+        returningTheArray = "n\tx0\txn+1\t#Rect";
+
+        for (String[] s : resultsToPrint) {
+            returningTheArray += "\n";
+            for(String s2 : s){
+                returningTheArray += s2 + "\t";
+            }
+
+        }
+        return returningTheArray;
+    }
+
+    private void setValuesOnArray(int i, int m, String n, String x0, String currentFraction, String xn1, String rectNumb) {
+        if (resultsToPrint != null) {
+            for (int j = 0; j < 5; j++) {
+
+                switch (j) {
+                    case 0:
+                        resultsToPrint[i][j] = n;
+                        break;
+                    case 1:
+                        resultsToPrint[i][j] = x0;
+                        break;
+                    case 2:
+                        resultsToPrint[i][j] = currentFraction;
+                        break;
+                    case 3:
+                        resultsToPrint[i][j] = xn1;
+                        break;
+                    case 4:
+                        resultsToPrint[i][j] = rectNumb;
+                        break;
+                }
+
+            }
+        } else {
+            resultsToPrint = new String[5][m];
+            for (int j = 0; j < 5; j++) {
+
+                switch (j) {
+                    case 0:
+                        resultsToPrint[i][j] = n;
+                        break;
+                    case 1:
+                        resultsToPrint[i][j] = x0;
+                        break;
+                    case 2:
+                        resultsToPrint[i][j] = currentFraction;
+                        break;
+                    case 3:
+                        resultsToPrint[i][j] = xn1;
+                        break;
+                    case 4:
+                        resultsToPrint[i][j] = rectNumb;
+                        break;
+                }
+
+            }
+        }
+
+    }
+
 }
--- src/modeladoysimulacion/ModeladoYSimulacion.java
+++ src/modeladoysimulacion/ModeladoYSimulacion.java
@@ -12,98 +12,97 @@ public class ModeladoYSimulacion {
     public static void main(String[] args) {
         char sel = '0', bi = 0;
         char cont;
-        
+
         new KolmogorovSmirnov(5).print().compareD(.409);
-        
+
         //PEstadistica();
-        
         Scanner scan = new Scanner(System.in);
-        do{
-            do{
-            bi = 0;
-            System.out.println("Que tipo de generador deseas:\n(Presione el num que desea y de enter)\n"
-                + "1.Generador Congruencial Mixto\n" + "2.Generador Congruencial Multiplicativo\n"
-                + "3.Generador Alternativo(propio)");
-            sel = scan.next().charAt(0);
-        switch(sel){
-            case '1':
-                GCMix();
-                break;
-            case '2':
-                GCMult();
-                break;
-            case '3':
-                GRndm();
-                break;
-            default:
-                System.out.println("Opcion no valda.");
-                bi = 1;
-        }}while(bi == 1);
-        
+        do {
+            do {
+                bi = 0;
+                System.out.println("Que tipo de generador deseas:\n(Presione el num que desea y de enter)\n"
+                        + "1.Generador Congruencial Mixto\n" + "2.Generador Congruencial Multiplicativo\n"
+                        + "3.Generador Alternativo(propio)");
+                sel = scan.next().charAt(0);
+                switch (sel) {
+                    case '1':
+                        GCMix();
+                        break;
+                    case '2':
+                        GCMult();
+                        break;
+                    case '3':
+                        GRndm();
+                        break;
+                    default:
+                        System.out.println("Opcion no valda.");
+                        bi = 1;
+                }
+            } while (bi == 1);
+
             System.out.println("Deseas generar de nuevo?\n1.Si\n2.No");
             cont = scan.next().charAt(0);
-            
-        }while(cont == '1');
-        
+
+        } while (cont == '1');
+
     }
 
     private static void GCMix() {
         Scanner scan = new Scanner(System.in);
-        try{
-        System.out.print("Ingresa A: ");
-        int a = scan.nextInt();
-        System.out.print("Ingresa X0: ");
-        int x = scan.nextInt();
-        System.out.print("Ingresa C: ");
-        int c = scan.nextInt();
-        System.out.print("Ingresa M: ");
-        int m = scan.nextInt();
-        GeneradorCongruencialMixto mix1 = new GeneradorCongruencialMixto(a, x, c, m);
-        mix1.makeThisHappen();
-        }catch(Exception e){
-            System.out.println("Error");
+        try {
+            System.out.print("Ingresa A: ");
+            int a = scan.nextInt();
+            System.out.print("Ingresa X0: ");
+            int x = scan.nextInt();
+            System.out.print("Ingresa C: ");
+            int c = scan.nextInt();
+            System.out.print("Ingresa M: ");
+            int m = scan.nextInt();
+            GeneradorCongruencialMixto mix1 = new GeneradorCongruencialMixto(a, x, c, m);
+            mix1.makeThisHappen();
+            System.out.println(mix1.letsPrintThisShit());
+        } catch (Exception e) {
+            System.out.println("Error" + e);
             return;
         }
-        
-                                    
-        
+
     }
 
     private static void GCMult() {
         Scanner scan = new Scanner(System.in);
-        try{
-        System.out.print("Ingresa A: ");
-        int a = scan.nextInt();
-        System.out.print("Ingresa X0: ");
-        int x = scan.nextInt();
-        System.out.print("Ingresa M: ");
-        int m = scan.nextInt();
-        new GCMult(a, x, m).init();
-        }catch(Exception e){
+        try {
+            System.out.print("Ingresa A: ");
+            int a = scan.nextInt();
+            System.out.print("Ingresa X0: ");
+            int x = scan.nextInt();
+            System.out.print("Ingresa M: ");
+            int m = scan.nextInt();
+            new GCMult(a, x, m).init();
+        } catch (Exception e) {
             System.out.println("Error");
             return;
         }
-        
+
     }
 
     private static void GRndm() {
         Scanner scan = new Scanner(System.in);
         int size;
         System.out.println("Cuantos numeros quieres generar?(indique el num y de enter)");
-        try{
+        try {
             size = scan.nextInt();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error");
             return;
         }
         GRandom rA = new GRandom();
         double[] rndL = rA.getRndL(size);
-        for(int i = 1; i < size + 1; i++){
+        for (int i = 1; i < size + 1; i++) {
             System.out.println("Dato " + i + " : " + rndL[i - 1]);
         }
     }
-    
-    private static void PEstadistica(){
+
+    private static void PEstadistica() {
         new Estadisticas(5).compareZ(1.96);
     }
 }
